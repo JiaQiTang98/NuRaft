b76a8cadc1ca3cf029f8983375d8444d11ef900f
“JiaQi <2963103258@qq.com>
2026-01-26 15:47:43 +0800

    Reopen the peer if it was shutdown previously

--- src/peer.cxx
+++ src/peer.cxx
@@ -313,6 +313,7 @@ bool peer::recreate_rpc(ptr<srv_config>& config,
 }
 
 void peer::shutdown() {
+    p_db("call peer %d shutdown", get_id());
     // Should set the flag to block all incoming requests.
     abandoned_ = true;
 
@@ -324,6 +325,29 @@ void peer::shutdown() {
         rpc_.reset();
     }
     hb_task_.reset();
+    is_shutdown_ = true;
+}
+
+
+void peer::reopen(context& ctx, timer_task<int32>::executor& hb_exec) {
+    p_db("call peer %d reopen", get_id());
+    // Should set the flag to block all incoming requests.
+    abandoned_ = false;
+
+    // Cut off all shared pointers related to ASIO and Raft server.
+    scheduler_ = ctx.scheduler_;
+    // {   // To guarantee atomic reset
+    //     // (race between send_req()).
+    //     std::lock_guard<std::mutex> l(rpc_protector_);
+    //     recreate_rpc(config_, ctx);
+    // }
+    hb_task_ = cs_new< timer_task<int32>,
+                            timer_task<int32>::executor&,
+                            int32 >
+                          ( hb_exec, config_->get_id(),
+                            timer_task_type::heartbeat_timer ) ;
+    is_shutdown_ = false;
+    p_db("call peer %d reopen successed", get_id());
 }
 
 } // namespace nuraft;
